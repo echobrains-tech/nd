6995bfdf2d3101509639e4116de536b2af039b92
Nagaraju Pagigi <pnagaraju3001@gmail.com>
2026-06-30 14:35:58 +0530

    first stage

---
+++ src/main/java/com/config/app/DnApplication.java
@@ -0,0 +1,13 @@
+package com.config.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/config/app/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.config.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	
+	@GetMapping("/")
+	private String welcome() {
+		return """
+				************************************ \n
+                Welcome to ND Application : 3384
+                ************************************
+
+                Available APIs
+
+                GET    /view/{id}
+                POST   /create
+                PUT    /update/{id}
+                DELETE /delete/{id}
+
+                Version : 1.0
+                Status  : Running Successfully
+				""";
+	}
+	
+	@GetMapping("/created-by")
+	public String createdBy() {
+		return "RAJU - 30/06/2026";
+	}
+
+}
